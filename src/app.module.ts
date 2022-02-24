3f9c5c14ae7f2275bd834fd9a9a412a38fe9cb11
Demba Diack <dembadiack@outlook.com>
2022-02-24 05:13:47 +0000

    a

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import {ServeStaticModule} from "@nestjs/serve-static";
 
 
 let serveStatic = ServeStaticModule.forRoot({
-  rootPath: resolve(__dirname, "Client", "build", "index.html"),
+  rootPath: join(__dirname, '..', 'client'),
   });
 let imports = [ProductsModule,UsersModule,MongooseModule.forRoot(CONFIG.db_uri)];
 if(process.env.NODE_ENV == "production")
