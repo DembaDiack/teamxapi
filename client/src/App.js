ac2c5c5bd795d3fb50599ba0bee19fb1742bb342
Demba Diack <dembadiack@outlook.com>
2022-02-25 13:34:30 +0000

    a

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,6 @@ import Navbar from "./components/navbar/navbar";
 import Products from "./components/products/products";
 import Home from "./components/home/home";
 import Users from "./components/users/users";
-import Footer from "./components/footer/footer";
 
 function App() {
 
@@ -25,7 +24,6 @@ function App() {
         <Route exact path='/products/*' element={<Products/>} />
         <Route exact path='/users/*' element={<Users/>} />
       </Routes>
-      <Footer/>
       </div>
     </Router>
   );
